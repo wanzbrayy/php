2bd9068d4bfc0243882f2d1e2774976f1ff3a314
Awan Berlian <berlianawan7@gmail.com>
2025-01-12 01:17:09 +0700

    Create endroid-qr-code.php

---
+++ endroid-qr-code.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Endroid\QrCode;
+
+use Endroid\QrCode\Writer\PngWriter;
+
+class QrCode
+{
+    private $text;
+    private $size = 300;
+    private $encoding = 'UTF-8';
+
+    public function __construct($text)
+    {
+        $this->text = $text;
+    }
+
+    public function setSize($size)
+    {
+        $this->size = $size;
+    }
+
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    public function getSize()
+    {
+        return $this->size;
+    }
+
+    public function getEncoding()
+    {
+        return $this->encoding;
+    }
+
+    public function write(PngWriter $writer)
+    {
+        return $writer->write($this);
+    }
+}
+
+class PngWriter
+{
+    public function write(QrCode $qrCode)
+    {
+        // Logika untuk membuat gambar PNG QR Code
+        $qrCodeImage = imagecreate($qrCode->getSize(), $qrCode->getSize());
+        $backgroundColor = imagecolorallocate($qrCodeImage, 255, 255, 255);
+        $blackColor = imagecolorallocate($qrCodeImage, 0, 0, 0);
+
+        // Menambahkan logika untuk menggambar QR Code (implementasi penyederhanaan)
+        // Dalam penerapan sesungguhnya, Anda akan membutuhkan generator QR yang lebih kompleks
+
+        // Menyimpan QR Code sebagai gambar PNG
+        ob_start();
+        imagepng($qrCodeImage);
+        $imageData = ob_get_clean();
+        imagedestroy($qrCodeImage);
+
+        return new PngImage($imageData);
+    }
+}
+
+class PngImage
+{
+    private $imageData;
+
+    public function __construct($imageData)
+    {
+        $this->imageData = $imageData;
+    }
+
+    public function getString()
+    {
+        return $this->imageData;
+    }
+}
+?>
