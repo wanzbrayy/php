f4a7243e3aa29c5b6ea7569716e4c88446c2e411
Awan Berlian <berlianawan7@gmail.com>
2025-01-12 01:46:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
-include 'functions.php';
-if (file_exists('autoload.php')) {
-    require 'autoload.php'; // Memuat autoloader manual jika file tersedia
-} else {
-    die('Autoload file not found. Please make sure autoload.php is present.');
-}
+// Memuat autoload.php yang berfungsi untuk autoload class dan file lainnya
+require_once __DIR__ . '/autoload.php';  // Pastikan autoload.php berada di lokasi yang benar
+
+// Memuat functions.php untuk menggunakan fungsi-fungsi yang telah didefinisikan
+include_once __DIR__ . '/functions.php';  // Pastikan functions.php berada di direktori yang benar
 
 use Endroid\QrCode\QrCode;
 use Endroid\QrCode\Writer\PngWriter;
@@ -17,13 +16,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name = 'KEPO LOE';
 
     // Membuat URL DANA untuk pembayaran
-    $danaPaymentUrl = "https://link.dana.id/qr/$danaNumber/$amount"; // URL DANA
+    $danaPaymentUrl = "https://link.dana.id/qr/$danaNumber/$amount";  // Pastikan format URL sesuai dengan API DANA
 
     // Membuat QR Code dari URL DANA
     $qrCode = new QrCode($danaPaymentUrl);
     $writer = new PngWriter();
     $qrCode->setSize(300); // Ukuran QR code
-    $image = $writer->write($qrCode); // Menyimpan gambar QR Code
+    $image = $writer->write($qrCode);  // Menyimpan gambar QR Code
 
     // Menyimpan gambar QR ke file
     $qrFilePath = 'qrcode.png';
