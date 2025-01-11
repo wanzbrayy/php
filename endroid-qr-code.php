3ec7f97e63116978aebabd2f699dd3eca390cf81
Awan Berlian <berlianawan7@gmail.com>
2025-01-12 02:39:57 +0700

    Update endroid-qr-code.php

--- endroid-qr-code.php
+++ endroid-qr-code.php
@@ -1,39 +1,51 @@
 <?php
+
 class QrCode
 {
     private $text;
     private $size = 300;
     private $encoding = 'UTF-8';
+
     public function __construct($text)
     {
         $this->text = $text;
     }
+
     public function setSize($size)
     {
         $this->size = $size;
     }
+
     public function getText()
     {
         return $this->text;
     }
+
     public function getSize()
     {
         return $this->size;
     }
+
     public function getEncoding()
     {
         return $this->encoding;
     }
+
     public function write(PngWriter $writer)
     {
         return $writer->write($this);
     }
 }
+
 class PngWriter
 {
     public function write(QrCode $qrCode)
     {
-        // Logika untuk membuat gambar PNG QR Code
+        // Pastikan ekstensi GD diaktifkan
+        if (!function_exists('imagecreate')) {
+            throw new Exception('Fungsi GD imagecreate tidak ditemukan. Pastikan ekstensi GD diaktifkan.');
+        }
+
         $qrCodeImage = imagecreate($qrCode->getSize(), $qrCode->getSize());
         $backgroundColor = imagecolorallocate($qrCodeImage, 255, 255, 255);
         $blackColor = imagecolorallocate($qrCodeImage, 0, 0, 0);
