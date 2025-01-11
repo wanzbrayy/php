cad21835fe34930987995e38fb5075a157816d9f
Awan Berlian <berlianawan7@gmail.com>
2025-01-12 00:39:05 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// Konfigurasi PayPal Sandbox
+return [
+    'client_id' => 'AZeb4u8BMni9caWHIWS1pxMZLLgiArqWMv45dRLw0VN_8Qth8MyOqWmDMDZ4COeclZV7YjUzj72olXsF',
+    'client_secret' => 'EFUN8zTG4CklE3fN8Lw8ygtsPNWS272GhRTj-VneTKkcOeGj5mYc4CfuGTSSJgJuEib7G5PepmtpfPit',
+    'api_base_url' => 'https://api-m.sandbox.paypal.com',
+    'return_url' => 'http://localhost/success.php',
+    'cancel_url' => 'http://localhost/cancel.php'
+];
